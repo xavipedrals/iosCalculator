afae04fa384652d8fa8627e3860c359fff208786
mac33 <mac33@a5s104mac23.fib.upc.es>
2016-01-26 17:57:53 +0100

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultatLabel: UILabel!
+    
+    var displayNumbers:Bool = false;
+    
+    var operandStack:[Double] = [Double]();
+    
+    //Variable computada
+    var displayValue:Double{
+        get{
+            let text = resultatLabel.text!
+            return Double(text)!
+        }
+        set{
+            resultatLabel.text = String(newValue)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +38,45 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func appendDigitToDisplay(sender: UIButton) {
+        if let digit = sender.currentTitle {
+            if displayNumbers {
+                resultatLabel.text = resultatLabel.text! + digit;
+            } else {
+                resultatLabel.text = "";
+                resultatLabel.text = resultatLabel.text! + digit;
+                displayNumbers = true;
+            }
+        }
+        
+    }
+    @IBAction func enter() {
+        displayNumbers = false
+        operandStack.append(displayValue)
+        
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if displayNumbers {
+            enter()
+        }
+        switch operation{
+            case "x":
+                performOperation{ $0 * $1 }
+            case "x":
+                performOperation{ $0 * $1 }
+            case "x":
+                performOperation{ $0 * $1 }
+            case "x":
+                performOperation{ $0 * $1 }
+            default:break
+        }
+    }
+    
+    func performOperation(operation:(Double, Double) -> Double){
+        if operandStack.count >= 2{
+            //resultatLabel = operation(operandStack.removeLast(), operandStack.removeLast())
+        }
+    }
 }
 
